658eca6730468f6e1cfa382b7243bd891cd3d19a
Kyle <kkingsbe@gmail.com>
2018-04-16 20:56:32 -0400

    Fixing no combobox options

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -18,6 +23,9 @@ public class Controller {
     public int lines;
     public static String newline = System.getProperty("line.separator");
     public ComboBox aircraftSelector;
+    public ImageView aircraftImage;
+    public Label planeDescription;
+    public String[][] aircraft;
 
     public void initialize(){
 
@@ -33,9 +41,8 @@ public class Controller {
             Template = reader.readLine();
             lineLen = Template.split(",").length;
             System.out.println("Template file has " + lineLen + " fields");
-
+            aircraft = new String[lineLen][lines];
             allAircraftData = Files.readAllLines(Paths.get("Resources/aircraft")).toString();
-            //System.out.println(allAircraftData);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,7 +50,7 @@ public class Controller {
 
         lines = allAircraftData.split(";").length;
 
-        String [][] aircraft = new String[lineLen][lines];
+        aircraft = new String[lineLen][lines];
 
         for (int line = 1; line < lines; line++){
             String currentLine = null;
@@ -64,25 +71,52 @@ public class Controller {
 
         }
 
-        updateAircraftSelector(aircraft);
+    }
+
+    public void infoUpdate(ActionEvent actionEvent) {
+        updateImage(aircraftSelector.getValue().toString());
+        int plane = planeLookUp(aircraftSelector.getValue().toString());
+        updateAircraftData(aircraft[plane]);
+    }
+
+    private int planeLookUp(String planeString) {
+        int planeIndex = 0;
+        for (int x = 1; x < lines;x++){
+            if (aircraft[x][1] == planeString){
+                planeIndex = x;
+                break;
+            }
+        }
+        return planeIndex;
+    }
+
+    private void updateAircraftData(String[] aircraft) {
+        String labelText = null;
+        try {
+            System.out.println("combo");
+            aircraftSelector.getItems().addAll(aircraft[1]);
+        } catch (java.lang.ArrayIndexOutOfBoundsException e) {}
+
+        for (int attribute = 1; attribute < aircraft.length;attribute++){
+            try {
+                labelText = labelText + aircraft[attribute];
+            } catch (java.lang.ArrayIndexOutOfBoundsException ignored){}
+        }
+        labelText += "\n";
+
+        planeDescription.setText(labelText);
 
     }
 
-    private void updateAircraftSelector(String[][] aircraft) {
-        String labelText = null;
-        for (int plane = 1;plane<lines - 1;plane++){
-            System.out.println(plane);
-            try {
-                aircraftSelector.getItems().addAll(aircraft[plane][1]);
-            } catch (java.lang.ArrayIndexOutOfBoundsException e) {}
-
-            for (int attribute = 1; attribute < aircraft.length;attribute++){
-                try {
-                    labelText = labelText + aircraft[plane][attribute];
-                } catch (java.lang.ArrayIndexOutOfBoundsException ignored){}
-            }
-            labelText += "\n";
+    private void updateImage(String plane) {
+        File file = new File("Resources/Images/"+plane+".jpg");
+        System.out.println(file);
+        Image airplane = null;
+        try {
+            airplane = new Image(file.toURI().toURL().toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
-        labelText = labelText.replace("null","").replace("[","").replace("]","\n");
+        aircraftImage.setImage(airplane);
     }
 }
